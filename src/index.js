929cb2ed90caeb0061ae194d58f559d35c93d7fe
KlaraGajaszek <klaragajaszek21@wp.pl>
2020-11-17 12:42:49 +0100

    add comment and redme

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './style/imports.scss';
-import ExampleButton from './Components/exampleButton'
+// get element from exampleButton.js
+import ExampleButton from './Components/exampleButton';
 
 const setup = () => {
+  //create new instance of our button
   new ExampleButton();
 };
 
