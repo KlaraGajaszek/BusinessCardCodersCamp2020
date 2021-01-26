c5cfb87fd6b65b8860bb42253be0f16da8dee6b8
wojaneta <wojaneta@gmail.com>
2021-01-26 22:03:56 +0100

    Knight possible moves defined

--- src/pieces/knight.ts
+++ src/pieces/knight.ts
@@ -6,10 +6,36 @@ class Knight extends Piece {
         // this.name = name;
         this.display = `<i class="fas fa-chess-knight ${side}"></i>`; //fontawesome knight
     }
-    // findLegalMoves() {
-    //     const possibleMoves = [];
+    findLegalMoves() {
+        const possibleMoves: Array<string> = [];
 
-    //     return possibleMoves;
+        if (this.x+1 <=7 && this.x+1 >= 0 && this.y+2 <= 7 && this.y+2 >= 0) {
+            possibleMoves.push(`${this.x+1},${this.y+2}`)
+        }
+        if (this.x+2 <=7 && this.x+2 >= 0 && this.y+1 <= 7 && this.y+1 >= 0) {
+            possibleMoves.push(`${this.x+2},${this.y+1}`)
+        }
+        if (this.x+2 <=7 && x+2 >= 0 && this.y-1 <= 7 && this.y-1 >= 0) {
+            possibleMoves.push(`${this.x+2},${this.y-1}`)
+        }
+        if (this.x+1 <=7 && this.x+1 >= 0 && this.y-2 <= 7 && this.y-2 >= 0) {
+            possibleMoves.push(`${this.x+1},${this.y-2}`)
+        }
+        if (this.x-1 <=7 && this.x-1 >= 0 && this.y-2 <= 7 && this.y-2 >= 0) {
+            possibleMoves.push(`${this.x-1},${this.y-2}`)
+        }
+        if (this.x-2 <=7 && this.x-2 >= 0 && this.y-1 <= 7 && this.y-1 >= 0) {
+            possibleMoves.push(`${this.x-2},${this.y-1}`)
+        }
+        if (this.x-2 <=7 && this.x-2 >= 0 && this.y+1 <= 7 && this.y+1 >= 0) {
+            possibleMoves.push(`${this.x-2},${this.y+1}`)
+        }
+        if (this.x-1 <=7 && this.x-1 >= 0 && this.y+2 <= 7 && this.y+2 >= 0) {
+            possibleMoves.push(`${this.x-1},${this.y+2}`)
+        }
+    
+
+        return possibleMoves;
     // }
 }
 
