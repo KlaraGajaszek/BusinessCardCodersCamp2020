6803b7a14d91719835617d79c784d4f03195d66e
Szymon Suchanowski <szymonsuchanowski@gmail.com>
2021-01-28 20:21:26 +0100

    feat: rook movement

--- src/pieces/rook.ts
+++ src/pieces/rook.ts
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import { Field } from '../types';
 
 class Rook extends Piece {
   constructor(x: number, y: number, readonly side: string) {
@@ -6,11 +7,28 @@ class Rook extends Piece {
     // this.name = name;
     this.display = `<i class="fas fa-chess-rook ${side}"></i>`; //fontawesome rook
   }
-  // findLegalMoves() {
-  //     const possibleMoves = [];
 
-  //     return possibleMoves;
-  // }
+  findLegalMoves(board: Field[][]): string[] {
+    const possibleMoves: Array<string> = [];
+
+    for (let i = 1; this.x - i > -1 && !board[this.x - i][this.y]; i++) {
+      possibleMoves.push(`${this.x - i},${this.y}`);
+    }
+
+    for (let i = 1; this.x + i < 8 && !board[this.x + i][this.y]; i++) {
+      possibleMoves.push(`${this.x + i},${this.y}`);
+    }
+
+    for (let i = 1; this.y - i > -1 && !board[this.x][this.y - i]; i++) {
+      possibleMoves.push(`${this.x},${this.y - i}`);
+    }
+
+    for (let i = 1; this.y + i < 8 && !board[this.x][this.y + i]; i++) {
+      possibleMoves.push(`${this.x},${this.y + i}`);
+    }
+
+    return possibleMoves;
+  }
 }
 
 export default Rook;
