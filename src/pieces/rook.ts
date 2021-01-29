6a146679b7f9b6748e01f62ac5dc6a15ad3b0028
KlaraGajaszek <klaragajaszek21@wp.pl>
2021-01-29 23:11:22 +0100

    new idea

--- src/pieces/rook.ts
+++ src/pieces/rook.ts
@@ -8,57 +8,54 @@ class Rook extends Piece {
     this.display = `<i class="fas fa-chess-rook ${side}"></i>`; //fontawesome rook
   }
 
-  findLegalMoves(board: Field[][]): string[] {
-    const possibleMoves: Array<string> = [];
 
-    for (let i = 1; this.x - i > -1; i++) {
-      if (!board[this.x - i][this.y]) {
-        possibleMoves.push(`${this.x - i},${this.y}`);
-      } else if (board[this.x - i][this.y]?.side !== this.side) {
-        possibleMoves.push(`${this.x - i},${this.y}`);
-        break;
-      } else break;
-    }
-
-    for (let i = 1; this.x + i < 8; i++) {
-      if (!board[this.x + i][this.y]) {
-        possibleMoves.push(`${this.x + i},${this.y}`);
-      } else if (board[this.x + i][this.y]?.side !== this.side) {
-        possibleMoves.push(`${this.x + i},${this.y}`);
-        break;
-      } else break;
-    }
-
-    for (let i = 1; this.y - i > -1; i++) {
-      if (!board[this.x][this.y - i]) {
-        possibleMoves.push(`${this.x},${this.y - i}`);
-      } else if (board[this.x][this.y - i]?.side !== this.side) {
-        possibleMoves.push(`${this.x},${this.y - i}`);
-        break;
-      } else break;
-    }
-
-    for (let i = 1; this.y + i < 8; i++) {
-      if (!board[this.x][this.y + i]) {
-        possibleMoves.push(`${this.x},${this.y + i}`);
-      } else if (board[this.x][this.y + i]?.side !== this.side) {
-        possibleMoves.push(`${this.x},${this.y + i}`);
-        break;
-      } else break;
+  findLegalMoves(board: Field[][]): string {
+    const possibleMoves = [];
+    const attackingMoves = this.findAttackingMoves(board);
+    for (let move of attackingMoves) {
+      if (board[move.charAt(0)][move.charAt(2)] && board[move.charAt(0)][move.charAt(2)].side === this.side) continue;
+      possibleMoves.push(move);
     }
     return possibleMoves;
   }
 
-  findAttackingMoves(board: Field[][]): string[] {
-    const attackingMoves: Array<string> = [];
-    let possibleMoves = this.findLegalMoves(board);
-    for (let i = 0; i < possibleMoves.length; i++) {
-      let move = possibleMoves[i].split(',');
-      const x = parseInt(move[0]);
-      const y = parseInt(move[1]);
-      if (board[x][y] && board[x][y]?.side !== this.side) {
-        attackingMoves.push(possibleMoves[i]);
+  findAttackingMoves(board: Field[][]): string {
+    const attackingMoves = [];
+
+    for (let u = 1; u <= 7; u++) {
+      if (this.y - u < 0) break;
+      if (board[this.x][this.y - u]) {
+        if (board[this.x][this.y - u]) attackingMoves.push(`${this.x},${this.y - u}`);
+        break;
       }
+      attackingMoves.push(`${this.x},${this.y - u}`);
+    }
+
+    for (let d = 1; d <= 7; d++) {
+      if (this.y + d > 7) break;
+      if (board[this.x][this.y + d]) {
+        if (board[this.x][this.y + d]) attackingMoves.push(`${this.x},${this.y + d}`);
+        break;
+      }
+      attackingMoves.push(`${this.x},${this.y + d}`);
+    }
+
+    for (let l = 1; l <= 7; l++) {
+      if (this.x - l < 0) break;
+      if (board[this.x - l][this.y]) {
+        if (board[this.x - l][this.y]) attackingMoves.push(`${this.x - l},${this.y}`);
+        break;
+      }
+      attackingMoves.push(`${this.x - l},${this.y}`);
+    }
+
+    for (let r = 1; r <= 7; r++) {
+      if (this.x + r > 7) break;
+      if (board[this.x + r][this.y]) {
+        if (board[this.x + r][this.y]) attackingMoves.push(`${this.x + r},${this.y}`);
+        break;
+      }
+      attackingMoves.push(`${this.x + r},${this.y}`);
     }
     return attackingMoves;
   }
