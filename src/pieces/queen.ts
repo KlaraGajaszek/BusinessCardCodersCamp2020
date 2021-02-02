e925fafb6d59265d73749966bd8de4bea71bb752
Wojtek Warmuz <wwarmuz@gmail.com>
2021-02-02 13:20:18 +0100

    Add queen moves

--- src/pieces/queen.ts
+++ src/pieces/queen.ts
@@ -1,93 +1,105 @@
 import Piece from './piece';
-import board from '../board';
+import Board from '../board';
+import Field from '../field';
 
 class Queen extends Piece {
-    constructor(x: number, y: number, protected side: string) {
-        super(x, y, side);
-        // this.name = name;
-        this.display = `<i class="fas fa-chess-queen ${side}"></i>`; //fontawesome queen
+    _display: string;
+
+    constructor(public side: string) {
+        super(side);
+        this._display = `<i class="fas fa-chess-queen ${side}"></i>`;
     }
 
-    findLegalMoves() {
-        const possibleMoves: Array<string | null> = [];
-        for (let i = 1; this.y + i < 8 && !board[this.x][this.y + i]; i++) {
-            possibleMoves.push(`${this.x},${this.y + i}`);
+    findLegalMoves(board: Board, actualField: Field): string[] {
+        const possibleMoves: string[] = new Array();
+        for (let r = 1; actualField.y + r < 8 && board.fields[actualField.x][actualField.y + r].piece === null; r++) {
+            possibleMoves.push(`${actualField.x},${actualField.y + r}`);
         };
-        for (let i = 1; this.y - i >= 0 && !board[this.x][this.y - i]; i++) {
-            possibleMoves.push(`${this.x},${this.y - i}`);
+        for (let l = 1; actualField.y - l >= 0 && board.fields[actualField.x][actualField.y - l].piece === null; l++) {
+            possibleMoves.push(`${actualField.x},${actualField.y - l}`);
         };
-        for (let i = 1; this.x + i < 8 && !board[this.x + i][this.y]; i++) {
-            possibleMoves.push(`${this.x + i},${this.y}`);
+        for (let d = 1; actualField.x + d < 8 && board.fields[actualField.x + d][actualField.y].piece === null; d++) {
+            possibleMoves.push(`${actualField.x + d},${actualField.y}`);
         };
-        for (let i = 1; this.x - i >= 0 && !board[this.x - i][this.y]; i++) {
-            possibleMoves.push(`${this.x - i},${this.y}`);
+        for (let u = 1; actualField.x - u >= 0 && board.fields[actualField.x - u][actualField.y].piece === null; u++) {
+            possibleMoves.push(`${actualField.x - u},${actualField.y}`);
         };
-        for (let i = 1; this.y + i <= 7 && this.x - i >= 0 && !board[this.x - i][this.y + i]; i++) {
-            possibleMoves.push(`${this.x - i},${this.y + i}`);
+        for (let ru = 1; actualField.y + ru <= 7 && actualField.x - ru >= 0 && board.fields[actualField.x - ru][actualField.y + ru].piece === null; ru++) {
+            possibleMoves.push(`${actualField.x - ru},${actualField.y + ru}`);
         };
-        for (let i = 1; this.y + i <= 7 && this.x + i <= 7 && !board[this.x + i][this.y + i]; i++) {
-            possibleMoves.push(`${this.x + i},${this.y + i}`);
+        for (let rd = 1; actualField.y + rd <= 7 && actualField.x + rd <= 7 && board.fields[actualField.x + rd][actualField.y + rd].piece === null; rd++) {
+            possibleMoves.push(`${actualField.x + rd},${actualField.y + rd}`);
         };
-        for (let i = 1; this.y - i >= 0 && this.x + i <= 7 && !board[this.x + i][this.y - i]; i++) {
-            possibleMoves.push(`${this.x + i},${this.y - i}`);
+        for (let ld = 1; actualField.y - ld >= 0 && actualField.x + ld <= 7 && board.fields[actualField.x + ld][actualField.y - ld].piece === null; ld++) {
+            possibleMoves.push(`${actualField.x + ld},${actualField.y - ld}`);
         };
-        for (let i = 1; this.y - i >= 0 && this.x - i >= 0 && !board[this.x - i][this.y - i]; i++) {
-            possibleMoves.push(`${this.x - i},${this.y - i}`);
+        for (let lu = 1; actualField.y - lu >= 0 && actualField.x - lu >= 0 && board.fields[actualField.x - lu][actualField.y - lu].piece === null; lu++) {
+            possibleMoves.push(`${actualField.x - lu},${actualField.y - lu}`);
         };
         return possibleMoves;
     }
-    findAttackingMoves() {
+    findAttackingMoves(board: Board, actualField: Field): Array<string | null> {
         const attackingMoves: Array<string | null> = [];
-        for (let i = 1; this.y + i < 8; i++) {
-            if (board[this.x][this.y + i] && board[this.x][this.y + i]?.side !== this.side) {
-                attackingMoves.push(`${this.x},${this.y + i}`);
+        for (let r = 1; actualField.y + r < 8; r++) {
+            if (board.fields[actualField.x][actualField.y + r].piece !== null) {
+                if (board.fields[actualField.x][actualField.y + r]?.piece!.side !== actualField.piece!.side)
+                    attackingMoves.push(`${actualField.x},${actualField.y + r}`);
                 break;
             }
         };
-        for (let i = 1; this.y - i >= 0; i++) {
-            if (board[this.x][this.y - i] && board[this.x][this.y - i]?.side !== this.side) {
-                attackingMoves.push(`${this.x},${this.y - i}`);
+        for (let l = 1; actualField.y - l >= 0; l++) {
+            if (board.fields[actualField.x][actualField.y - l].piece !== null) {
+                if (board.fields[actualField.x][actualField.y - l]?.piece!.side !== actualField.piece!.side)
+                    attackingMoves.push(`${actualField.x},${actualField.y - l}`);
                 break;
             };
         };
-        for (let i = 1; this.x + i < 8; i++) {
-            if (board[this.x + i][this.y] && board[this.x + i][this.y]?.side !== this.side) {
-                attackingMoves.push(`${this.x + i},${this.y}`);
+        for (let d = 1; actualField.x + d < 8; d++) {
+            if (board.fields[actualField.x + d][actualField.y].piece !== null) {
+                if (board.fields[actualField.x + d][actualField.y]?.piece!.side !== actualField.piece!.side)
+                    attackingMoves.push(`${actualField.x + d},${actualField.y}`);
                 break;
             }
         };
-        for (let i = 1; this.x - i >= 0; i++) {
-            if (board[this.x - i][this.y] && board[this.x - i][this.y]?.side !== this.side) {
-                attackingMoves.push(`${this.x - i},${this.y}`);
+        for (let u = 1; actualField.x - u >= 0; u++) {
+            if (board.fields[actualField.x - u][actualField.y].piece !== null) {
+                if (board.fields[actualField.x - u][actualField.y]?.piece!.side !== actualField.piece!.side)
+                    attackingMoves.push(`${actualField.x - u},${actualField.y}`);
+                break;
+
+            }
+        };
+        for (let ru = 1; actualField.y + ru <= 7 && actualField.x - ru >= 0; ru++) {
+            if (board.fields[actualField.x - ru][actualField.y + ru].piece !== null) {
+                if (board.fields[actualField.x - ru][actualField.y + ru]?.piece!.side !== actualField.piece!.side)
+                    attackingMoves.push(`${actualField.x - ru},${actualField.y + ru}`);
                 break;
             }
         };
-        for (let i = 1; this.y + i <= 7 && this.x - i >= 0; i++) {
-            if (board[this.x - i][this.y + i] && board[this.x - i][this.y + i]?.side !== this.side) {
-                attackingMoves.push(`${this.x - i},${this.y + i}`)
+        for (let rd = 1; actualField.y + rd <= 7 && actualField.x + rd <= 7; rd++) {
+            if (board.fields[actualField.x + rd][actualField.y + rd].piece !== null) {
+                if (board.fields[actualField.x + rd][actualField.y + rd]?.piece!.side !== actualField.piece!.side)
+                    attackingMoves.push(`${actualField.x + rd},${actualField.y + rd}`);
                 break;
             }
         };
-        for (let i = 1; this.y + i <= 7 && this.x + i <= 7; i++) {
-            if (board[this.x + i][this.y + i] && board[this.x + i][this.y + i]?.side !== this.side) {
-                attackingMoves.push(`${this.x + i},${this.y + i}`);
+        for (let ld = 1; actualField.y - ld >= 0 && actualField.x + ld <= 7; ld++) {
+            if (board.fields[actualField.x + ld][actualField.y - ld].piece !== null) {
+                if (board.fields[actualField.x + ld][actualField.y - ld]?.piece!.side !== actualField.piece!.side)
+                    attackingMoves.push(`${actualField.x + ld},${actualField.y - i}`);
                 break;
             }
         };
-        for (let i = 1; this.y - i >= 0 && this.x + i <= 7; i++) {
-            if (board[this.x + i][this.y - i] && board[this.x + i][this.y - i]?.side !== this.side) {
-                attackingMoves.push(`${this.x + i},${this.y - i}`);
-                break;
-            }
-        };
-        for (let i = 1; this.y - i >= 0 && this.x - i >= 0; i++) {
-            if (board[this.x - i][this.y - i] && board[this.x - i][this.y - i]?.side !== this.side) {
-                attackingMoves.push(`${this.x - i},${this.y - i}`);
+        for (let lu = 1; actualField.y - lu >= 0 && actualField.x - lu >= 0; lu++) {
+            if (board.fields[actualField.x - lu][actualField.y - lu].piece !== null) {
+                if (board.fields[actualField.x - lu][actualField.y - lu]?.piece!.side !== actualField.piece!.side)
+                    attackingMoves.push(`${actualField.x - lu},${actualField.y - lu}`);
                 break;
             }
         };
         return attackingMoves;
     }
 }
+
 export default Queen;
 
