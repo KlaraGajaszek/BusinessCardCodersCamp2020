34831bcb8783c14ce8a2c3c3c9378cc7853f6928
Kamil Dzierzak <kamdzierzak@gmail.com>
2021-02-03 21:24:40 +0100

    Add attacks to possible moves

--- src/pieces/pawn.ts
+++ src/pieces/pawn.ts
@@ -16,30 +16,44 @@ class Pawn extends Piece {
     const y = actualField.y;
     if (this.side == 'white') {
       //Two forward white
-      if (x === 6) {
-        if (board.fields[x - 2][y].isEmpty() && board.fields[x - 1][y].isEmpty()) {
-          possibleMoves.push(`${x - 2},${y}`);
-        }
+      if (x === 6 && board.fields[x - 2][y].isEmpty() && board.fields[x - 1][y].isEmpty()) {
+        possibleMoves.push(`${x - 2},${y}`);
       }
       //One forward white
-      if (x - 1 >= 0) {
-        if (board.fields[x - 1][y].isEmpty()) {
-          possibleMoves.push(`${x - 1},${y}`);
-        }
+      if (x - 1 >= 0 && board.fields[x - 1][y].isEmpty()) {
+        possibleMoves.push(`${x - 1},${y}`);
+      }
+      //Attacks white
+      if (board.fields[x - 1][y - 1] !== undefined 
+        && !board.fields[x - 1][y - 1].isEmpty()
+         && board.fields[x - 1][y - 1].piece?.side === 'black') {
+          possibleMoves.push(`${x - 1},${y - 1}`);
+      }
+      if (board.fields[x - 1][y + 1] !== undefined
+         && !board.fields[x - 1][y + 1].isEmpty()
+          && board.fields[x - 1][y + 1].piece?.side === 'black') {
+          possibleMoves.push(`${x - 1},${y + 1}`);
       }
     } else {
       // Two forward black
-      if (x === 1) {
-        if (board.fields[x + 2][y].isEmpty() && board.fields[x + 1][y].isEmpty()) {
-          possibleMoves.push(`${x + 2},${y}`);
-        }
+      if (x === 1 && board.fields[x + 2][y].isEmpty() && board.fields[x + 1][y].isEmpty()) {
+        possibleMoves.push(`${x + 2},${y}`);
       }
       // One forward black
-      if (x + 1 <= 7) {
-        if (board.fields[x + 1][y].isEmpty()) {
+      if (x + 1 <= 7 && board.fields[x + 1][y].isEmpty()) {
           possibleMoves.push(`${x + 1},${y}`);
-        }
       }
+      //Attacks black
+      if (board.fields[x + 1][y - 1] !== undefined
+        && !board.fields[x + 1][y - 1].isEmpty()
+         && board.fields[x + 1][y - 1].piece?.side === 'white') {
+         possibleMoves.push(`${x + 1},${y - 1}`);
+     }
+     if (board.fields[x + 1][y + 1] !== undefined
+        && !board.fields[x + 1][y + 1].isEmpty()
+         && board.fields[x + 1][y + 1].piece?.side === 'white') {
+         possibleMoves.push(`${x + 1},${y + 1}`);
+     }
     }
     return possibleMoves;
   }
@@ -50,26 +64,26 @@ class Pawn extends Piece {
     const y = actualField.y;
 
     if (this.side = 'white') {
-      if (board.fields[x - 1][y - 1] !== undefined) {
-        if (!board.fields[x - 1][y - 1].isEmpty() && board.fields[x - 1][y - 1].piece?.side === 'black') {
+      if (board.fields[x - 1][y - 1] !== undefined 
+        && !board.fields[x - 1][y - 1].isEmpty()
+         && board.fields[x - 1][y - 1].piece?.side === 'black') {
           attackingMoves.push(`${x - 1},${y - 1}`);
-          }
       }
-      if (board.fields[x - 1][y + 1] !== undefined) {
-        if (!board.fields[x - 1][y + 1].isEmpty() && board.fields[x - 1][y + 1].piece?.side === 'black') {
+      if (board.fields[x - 1][y + 1] !== undefined
+         && !board.fields[x - 1][y + 1].isEmpty()
+          && board.fields[x - 1][y + 1].piece?.side === 'black') {
           attackingMoves.push(`${x - 1},${y + 1}`);
-        }
       }
     } else {
-      if (board.fields[x + 1][y - 1] !== undefined) {
-        if (!board.fields[x + 1][y - 1].isEmpty() && board.fields[x + 1][y - 1].piece?.side === 'white') {
+      if (board.fields[x + 1][y - 1] !== undefined
+         && !board.fields[x + 1][y - 1].isEmpty()
+          && board.fields[x + 1][y - 1].piece?.side === 'white') {
           attackingMoves.push(`${x + 1},${y - 1}`);
-          }
       }
-      if (board.fields[x + 1][y + 1] !== undefined) {
-        if (!board.fields[x + 1][y + 1].isEmpty() && board.fields[x + 1][y + 1].piece?.side === 'white') {
+      if (board.fields[x + 1][y + 1] !== undefined
+         && !board.fields[x + 1][y + 1].isEmpty()
+          && board.fields[x + 1][y + 1].piece?.side === 'white') {
           attackingMoves.push(`${x + 1},${y + 1}`);
-        }
       }
     }
     return attackingMoves;
