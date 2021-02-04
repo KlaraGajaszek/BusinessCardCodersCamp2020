12ca48afda821271fc11b8fec2d6b2773b8b56e5
sebslon <sebastesterprogram@gmail.com>
2021-02-04 21:43:14 +0100

    Feat: Change turn after move

--- src/touched.ts
+++ src/touched.ts
@@ -1,6 +1,8 @@
 import Board from './board';
 import Field from './field';
 
+let whiteMove = true;
+
 const touched = (e: MouseEvent, board: Board) => {
     const target = e.currentTarget;
     if (target) {
@@ -12,30 +14,35 @@ const touched = (e: MouseEvent, board: Board) => {
             return;
         }
 
-        const possibleMoves = field.piece.findLegalMoves(board, field);
-        for (let move of possibleMoves) {
-            (document.getElementById(move) as HTMLDivElement).className += ` possibleMove`;
-            (document.getElementById(move) as HTMLDivElement).addEventListener('click', () => {
-                if (field.piece) {
-                    field.piece.move(field, board.getField(parseInt(move[0]), parseInt(move[2])));
-                }
-                for (let x = 0; x < board.boardSize; x++) {
-                    for (let y = 0; y < board.boardSize; y++) {
-                        (document.getElementById(`${x},${y}`) as HTMLDivElement).className = (document
-                            .getElementById(`${x},${y}`) as HTMLDivElement)
-                            .className.replace(`possibleMove`, '');
+        const turn = whiteMove ? 'white' : 'black';
 
-                        //TODO: rozwiązać tematykę event listenerów sprytniej, przenosząc każdy do osobnego pliku
-                        let old_element = document.getElementById(`${x},${y}`) as HTMLDivElement;
-                        let new_element = old_element.cloneNode(true);
-                        old_element.parentNode?.replaceChild(new_element, old_element);
-
-                        document.getElementById(`${x},${y}`)?.addEventListener('click', (e) => {
-                            touched(e, board);
-                        });
+        if(field.piece.side === turn) {
+            const possibleMoves = field.piece.findLegalMoves(board, field);
+            for (let move of possibleMoves) {
+                (document.getElementById(move) as HTMLDivElement).className += ` possibleMove`;
+                (document.getElementById(move) as HTMLDivElement).addEventListener('click', () => {
+                    if (field.piece) {
+                        field.piece.move(field, board.getField(parseInt(move[0]), parseInt(move[2])));
+                        whiteMove = !whiteMove
                     }
-                }
-            });
+                    for (let x = 0; x < board.boardSize; x++) {
+                        for (let y = 0; y < board.boardSize; y++) {
+                            (document.getElementById(`${x},${y}`) as HTMLDivElement).className = (document
+                                .getElementById(`${x},${y}`) as HTMLDivElement)
+                                .className.replace(`possibleMove`, '');
+    
+                            //TODO: rozwiązać tematykę event listenerów sprytniej, przenosząc każdy do osobnego pliku
+                            let old_element = document.getElementById(`${x},${y}`) as HTMLDivElement;
+                            let new_element = old_element.cloneNode(true);
+                            old_element.parentNode?.replaceChild(new_element, old_element);
+    
+                            document.getElementById(`${x},${y}`)?.addEventListener('click', (e) => {
+                                touched(e, board);
+                            });
+                        }
+                    }
+                });
+            }
         }
     }
 };
